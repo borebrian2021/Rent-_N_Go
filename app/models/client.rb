fa29c1bebf96ae4c285b2467daaa0b1da9ed6906
denisdelaki <denisdelaki@gmail.com>
2022-10-20 11:22:09 +0300

    created the client model and added the validations and its relationships with other models

---
+++ app/models/client.rb
@@ -0,0 +1,14 @@
+class Client < ApplicationRecord
+    #relationships 
+    has_many :properties
+    has_many :spaces, through: :properties 
+    has_many :messages 
+    has_many :reservations 
+    #validations 
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqeness: true 
+    validates :phone_number, presence: true, length: {maximum: 10}
+    validates :password, presence: true, length: {minimum: 4}
+    validates :password_confirmation, presence: true, length: {minimum: 4}
+end
