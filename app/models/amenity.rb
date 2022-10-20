5e3b36a0844e8baa9c9802cc65699d7678296d3d
Zac-Mwangi <zackmwangi998@gmail.com>
2022-10-20 16:28:56 +0300

    Done with the validations and associations part

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,2 +1,11 @@
 class Amenity < ApplicationRecord
+    belongs_to :space
+
+    # should we delete all the selected amenities onces an amenity is selected
+    # has_many :selected_amenities , dependent: :destroy
+    has_many :selected_amenities
+
+    validates :space_id, presence: true;
+    validates :item_name, presence: true
+    validates :price, presence: true;
 end
