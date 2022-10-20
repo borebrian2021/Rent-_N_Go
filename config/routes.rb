1ae81fbc3564406074058ca61b9ef35c4734744e
denisdelaki <denisdelaki@gmail.com>
2022-10-20 13:01:20 +0300

    Added the login route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :payments, only: [:index, :create, :show]
   resources :messages, only: [:index, :show, :create]
   resources :clients, only: [:create, :show]
+  post "/login", to: "sessions#create"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
