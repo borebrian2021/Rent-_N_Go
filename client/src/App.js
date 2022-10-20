c9b33214505dc734787a1f9a7a714cb6166b956f
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-10-20 20:10:41 +0300

    inclusion of the footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
+import Footer from './Components/Footer';
 
 // import TopBar from './Components/TopBar';
-import Contact from './Components/Contact';
+// import Contact from './Components/Contact';
 // import PropertyDetails from './Components/PropertyDetails';
 // import MySpaces from './Components/MySpaces';
 function App() {
   return (
     <div className="App">
      {/* <TopBar/> */}
-     <Contact/>
+     {/* <Contact/> */}
      {/* <PropertyDetails/> */}
      {/* <MySpaces/> */}
+     <Footer/>
     </div>
   );
 }
