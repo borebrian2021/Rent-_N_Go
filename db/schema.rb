8ef15aa46becaa55d9e76f2223d41f1dc87aeac2
denisdelaki <denisdelaki@gmail.com>
2022-10-20 12:17:40 +0300

    run the schema for the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_083439) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_085055) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -51,6 +51,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_083439) do
   create_table "messages", force: :cascade do |t|
     t.integer "client_id", null: false
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.integer "reservation_id", null: false
+    t.string "payment_method"
+    t.integer "total"
+    t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
