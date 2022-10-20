3d92debe6c4429bdff3800b51f2236a652652797
denisdelaki <denisdelaki@gmail.com>
2022-10-20 13:08:19 +0300

    Added the logging out route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :clients, only: [:create, :show]
   post "/login", to: "sessions#create"
   get "/me", to: "clients#show"
+  delete "/logout", to: "sessions#destroy"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
