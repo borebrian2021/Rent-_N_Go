4c0e05e9200050921505d15f5f318cfaf3030d6a
denisdelaki <denisdelaki@gmail.com>
2022-10-20 13:05:23 +0300

    Added staying Logged in route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :messages, only: [:index, :show, :create]
   resources :clients, only: [:create, :show]
   post "/login", to: "sessions#create"
+  get "/me", to: "clients#show"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
