ff784e7794e09713d8fbb4f51e6779c27f80f492
denisdelaki <denisdelaki@gmail.com>
2022-10-20 12:16:05 +0300

    added the index, show and create action methods

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,27 @@
+class PaymentsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+ def index
+        payment = Payment.all
+        render json: payment, status: :ok
+    end
+    def create
+        payment = Payment.create(payment_params)
+        render json: payment, status: :created
+    end
+    def show
+        payment = Payment.find(params[:id])
+        render json: payment    , status: :ok
+    end
+private
+    def payment_params
+        params.permit(:reservation_id, :payment_method, :total, :status)
+    end
+    def render_not_found_response
+        render json: { error: "Payment not found" }, status: :not_found
+    end
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+end
