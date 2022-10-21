100df3c578c21381c5ae5e58c087761469b5c72b
Sharon Mwenda <smkathambi@gmail.com>
2022-10-21 06:48:42 +0300

    added show route and controller for properties controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,17 @@
 class PropertiesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     def index
         properties = Property.all
         render json :properties
+    end
+
+    def show
+        property = Property.find(params[:id])
+        render json: :property
+    end
+
+    private
+    def render_not_found
+        render json: {error: 'Property not found'}, status: :not_found
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
 
   resources :reviews, only: [:index]
   resources :reservations, only: [:index]
-  resources :properties, only: [:index]
+  resources :properties, only: [:index, :show, :create, :destroy]
 end
