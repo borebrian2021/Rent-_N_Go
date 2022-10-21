a0de44ca09cd10a39da824b6f7af6fd92ac0338e
Sharon Mwenda <smkathambi@gmail.com>
2022-10-21 07:12:22 +0300

    adding create and destroy to propertiesContoller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -10,8 +10,23 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
         render json: :property
     end
 
+    def create
+       property = Property.create(property_params)
+       render json :property, status: :created 
+    end
+
+    def destroy
+        property = Property.find(params[:id])
+        property.destroy
+        head :no_content
+    end
+
     private
     def render_not_found
         render json: {error: 'Property not found'}, status: :not_found
+    end
+
+    def property_params
+        params.permit(:property_name, :location, :image_url)
     end
 end
