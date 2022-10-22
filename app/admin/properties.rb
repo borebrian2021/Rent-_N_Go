8873fa9bd712bfffdf827777c8e3adef152dc240
denisdelaki <denisdelaki@gmail.com>
2022-10-22 12:24:37 +0300

    specified the foreign key in the admin property model

--- app/admin/properties.rb
+++ app/admin/properties.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register Property do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
   permit_params :client_id, :property_name, :location, :image_url
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:client_id, :property_name, :location, :image_url]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
 end
