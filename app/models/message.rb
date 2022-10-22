4d7b1d3968058e19e81497dda5bc032c34ac5ce8
denisdelaki <denisdelaki@gmail.com>
2022-10-22 12:26:31 +0300

    modified the relationship synthax

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
     #relationships 
-    belongs_to: client
+    belongs_to :client
     #validations 
     validates :client_id, presence: true 
     validates :message, presence: true
