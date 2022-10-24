711fca02378a39e65c8b1520c5c9c1e2b2bc872a
Sharon Mwenda <smkathambi@gmail.com>
2022-10-24 07:26:34 +0300

    adding validations for property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,9 @@ class Property < ApplicationRecord
     belongs_to :client
     has_many :spaces
     has_many :reservations, through: :spaces
+
+    validates :property_name, presence: true
+    validates :location, presence: true
+    validates :image_url, presence: true
+
 end
