1cc6b4bd275cf6dcaa2b3d89c6aa7c71f8c6d6a9
Sharon Mwenda <smkathambi@gmail.com>
2022-10-24 07:20:05 +0300

    added show, create and destroy for reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,34 @@
 class ReservationsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     def index
         reservations = Reservation.all
         render json :reservations
     end
+
+    def show
+        reservation = Reservation.find(params[:id])
+        render json: :reservation
+    end
+
+    def create
+       reservation = Reservation.create(reservaion_params)
+       render json :reservation, status: :created 
+    end
+
+    def destroy
+        reservation = Reservation.find(params[:id])
+        reservation.destroy
+        head :no_content
+    end
+
+    private
+    def render_not_found
+        render json: {error: 'Reservation not found'}, status: :not_found
+    end
+
+    def reservation_params
+        params.permit(:space_id, :kickoff_date, :end_date, :total_cash, :client_id)
+    end
+
+
 end
