2cffd29612c11baea5224264a68977335dfd5cc6
Sharon Mwenda <smkathambi@gmail.com>
2022-10-24 07:47:07 +0300

    added validation to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
     belongs_to :space
     belongs_to :property
     belongs_to :client
+
+    validates :ratings, length: {in: 1..5}
+    validates :review, length: {maximum: 250}
 end
