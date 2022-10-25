c07524956dc84c1d42ea56b79f2d87ff237dc927
Zac-Mwangi <zackmwangi998@gmail.com>
2022-10-25 17:10:48 +0300

    Changes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,38 @@
-import "./App.css";
-import TopBar from "./Components/TopBar";
-import AfterHeader from "./Components/LandingPagComponents/AfterHeader";
-import LandingPage from "./Components/LandingPage";
-import SignUp from "./Components/SignUp";
-import Login from "./Components/Login";
-import ResetPassword from "./Components/ResetPassword";
-import TermsAndConditions from "./Components/TermsAndConditions";
-import Reservations from "./Components/DashboardComponents/Reservations";
-import Invoice from "./Components/Invoice";
-import Dashboard from "./Components/Dashboard";
-import PropertyCard from "./Components/PropertyListing";
-import PropertyDetails from "./Components/PropertyDetails";
-import MySpaces from "./Components/MySpaces";
-import PropertyListing from "./Components/PropertyListing";
+import './App.css';
+import TopBar from './Components/TopBar';
+import AfterHeader from './Components/LandingPagComponents/AfterHeader';
+import LandingPage from './Components/LandingPage';
+import SignUp from './Components/SignUp';
+import Login from './Components/Login';
+import ResetPassword from './Components/ResetPassword';
+import TermsAndConditions from './Components/TermsAndConditions';
+import Reservations from './Components/DashboardComponents/Reservations';
+import Invoice from './Components/Invoice';
+import Dashboard from './Components/Dashboard';
+import PropertyCard from './Components/PropertyListing';
+import PropertyDetails from './Components/PropertyDetails';
+import MySpaces from './Components/MySpaces';
+import PropertyListing from './Components/PropertyListing';
+import PropertyUploadForm from './Components/DashboardComponents/PropertyUploadForm';
+import SpaceUploadForm from './Components/DashboardComponents/SpaceUploadForm';
 function App() {
   return (
-    <div>
-      <TopBar />
-      {/* <Login/> */}
-
-      {/* <Reservations /> */}
-      {/* <Invoice /> */}
-      {/* <ResetPassword /> */}
-      {/* <SignUp /> */}
-      {/* <LandingPage/> */}
-      <Dashboard/>
-      {/* <PropertyListing /> */}
-      {/* <PropertyCard/> */}
-      {/* <PropertyDetails/> */}
-      {/* <MySpaces /> */}
-    </div>
+<div>
+  {/* <TopBar/> */}
+<Login/>
+ {/* <Reservations/> */}
+ {/* <Invoice/> */}
+ {/* <ResetPassword/> */}
+{/* <SignUp/> */}
+{/* <LandingPage/> */}
+{/* <Dashboard/> */}
+ {/* <PropertyListing/> */}
+ {/* <PropertyCard/> */}
+{/* <PropertyDetails/> */}
+{/* <MySpaces/> */}
+</div>
   );
 }
-
 export default App;
 {
   /* <Login/> */
