dd37ccf5fd339775ff6a3d5c2aea313c3c40b6cc
borebrian2021 <bkimutai2021@gmail.com>
2022-10-25 12:57:20 +0300

    Stable

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,29 @@ import SignUp from './Components/SignUp';
 import Login from './Components/Login';
 import ResetPassword from './Components/ResetPassword';
 import TermsAndConditions from './Components/TermsAndConditions';
-import Reservations from './Components/Reservations';
+import Reservations from './Components/DashboardComponents/Reservations';
 import Invoice from './Components/Invoice';
+import Dashboard from './Components/Dashboard';
+import PropertyCard from './Components/PropertyListing';
+import PropertyDetails from './Components/PropertyDetails';
+import MySpaces from './Components/MySpaces';
+import PropertyListing from './Components/PropertyListing';
 function App() {
   return (
 <div>
   {/* <TopBar/> */}
-{/* <Login/> */}
+<Login/>
 
  {/* <Reservations/> */}
  {/* <Invoice/> */}
  {/* <ResetPassword/> */}
 {/* <SignUp/> */}
-<LandingPage/>
+{/* <LandingPage/> */}
+{/* <Dashboard/> */}
+ {/* <PropertyListing/> */}
+ {/* <PropertyCard/> */}
+{/* <PropertyDetails/> */}
+{/* <MySpaces/> */}
 </div>
   );
 }
--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import DashboardCards from './DashboardComponents/DashboardCards';
+import DashboardFooter from './DashboardComponents/DashboardFooter';
+import Invoice from './Invoice';
+import Messages from './DashboardComponents/Messages';
+import MySpaces from './DashboardComponents/MySpaces';
+import Profile from './DashboardComponents/Profile';
+import Reservations from './DashboardComponents/Reservations';
+import Reviews from './DashboardComponents/Reviews';
+import SideBar from './DashboardComponents/SideBar';
 const Dashboard = () => {
     return (
-        <div>
-            
-        </div>
+        <section class="user-page section-padding">
+            <div class="container-fluid">
+                <div class="row">
+                    <SideBar />
+                    <div class="col-lg-9 col-md-12 col-xs-12 pl-0 user-dash2">
+                        <DashboardCards />
+                        <MySpaces />
+                        <Messages />
+                        <Reviews />
+                        <Profile />
+                        <Invoice/>
+                        <Reservations/>
+                        <DashboardFooter />
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
