25833ea05931aa766b76e23c7e22dbd87b9f55a3
Okumu Mark <marcokumu10@gmail.com>
2022-10-28 16:30:57 +0000

    added a useEffect for login feature

--- client/src/Components/GoogleLogin.js
+++ client/src/Components/GoogleLogin.js
@@ -1,20 +1,40 @@
 import React from "react";
-
+import { gapi } from "gapi-script";
 import { GoogleLogin } from "react-google-login";
+import { useEffect } from "react";
+// import {  useNavigate } from "react-router-dom";
 // refresh token
 import { refreshTokenSetup } from "./utils/refreshToken"; 
 
 const clientId =
   "1004992737804-trfkn3kml5mtsneum6cn5fvgn60g00mo.apps.googleusercontent.com";
 
+
+
 function LoginGoogle() {
   const onSuccess = (res) => {
     console.log("Login Success: currentUser:", res.profileObj);
+    
     alert(
       `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
     );
     refreshTokenSetup(res);
   };
+    // useNavigate("/dashboard");
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "email",
+        plugin_name: "Rent&Go",
+      });
+    }
+gapi.auth2.getAuthInstance(); 
+    // gapi.load("client:auth2", start);
+  }, []);
+
+  
 
   const onFailure = (res) => {
     console.log("Login failed: res:", res);
