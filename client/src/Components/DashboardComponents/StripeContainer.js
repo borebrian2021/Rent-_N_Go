25f85c16fdc6f31e636e00f5f1e35eb95ac45e22
Okumu Mark <marcokumu10@gmail.com>
2022-11-01 05:35:11 +0000

    written the stripe container code

---
+++ client/src/Components/DashboardComponents/StripeContainer.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import PaymentForm from "./PaymentForm";
+import { Container } from "react-bootstrap";
+
+const PUBLIC_KEY =
+  "pk_test_51LzCDEFcQNOREQ44bSJDbWRHcUoLMXwysFzjHZRWNjPUCT9CFisVEkP7kejHZ6F6schc5j0DONlQZr377QehsiAg00FkEtxCK0";
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
+
+function StripeContainer({ name, address, email, totalPrice }) {
+  console.log(parseInt(totalPrice));
+  const [clientSecret, setClientSecret] = useState("");
+  useEffect(() => {
+    fetch("/payment", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        amount: parseInt(`${totalPrice}00`),
+        currency: "usd",
+        name,
+        email,
+        address,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+  return (
+    <Container style={{ marginTop: "75px" }}>
+      <div className="App">
+        {clientSecret && (
+          <Elements options={options} stripe={stripeTestPromise}>
+            <PaymentForm />
+          </Elements>
+        )}
+      </div>
+    </Container>
+  );
+}
+
+export default StripeContainer;
