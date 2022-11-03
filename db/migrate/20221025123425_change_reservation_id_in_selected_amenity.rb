a7b8c3990357a7f8c1e9ff6aea0f4706b8e0822a
denisdelaki <denisdelaki@gmail.com>
2022-11-03 14:16:55 +0300

    changed the reservation_id column in selected amenities to use using keyword

--- db/migrate/20221025123425_change_reservation_id_in_selected_amenity.rb
+++ db/migrate/20221025123425_change_reservation_id_in_selected_amenity.rb
@@ -1,5 +1,5 @@
 class ChangeReservationIdInSelectedAmenity < ActiveRecord::Migration[7.0]
   def change
-    change_column(:selected_amenities, :reservation_id, :integer)
+    change_column(:selected_amenities, :reservation_id, :integer, using: 'reservation_id::integer')
   end
 end
